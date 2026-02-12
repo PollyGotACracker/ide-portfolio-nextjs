eb750880c267ca2aadb23db17ad49f4c38462143
PollyGotACracker <bitterns96@gmail.com>
2026-02-12 18:06:11 +0900

    style: add chevron to main section

--- app/(home)/_components/Main.tsx
+++ app/(home)/_components/Main.tsx
@@ -1,13 +1,24 @@
-// import styles from "./Main.module.css";
+import styles from "./Main.module.css";
+import { FaChevronDown } from "react-icons/fa6";
 
 export default function Main() {
   const name = process.env.NEXT_PUBLIC_NAME;
   const email = process.env.NEXT_PUBLIC_EMAIL;
 
   return (
-    <section>
+    <section className={styles.section}>
+      <div></div>
       <div>{name}</div>
       <div>{email}</div>
+      <ScrollChevron />
     </section>
+  );
+}
+
+function ScrollChevron() {
+  return (
+    <div className={styles.chevron}>
+      <FaChevronDown />
+    </div>
   );
 }
