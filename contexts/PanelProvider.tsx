95f1668e1599ccbe0f58773ef997528ea6a40575
PollyGotACracker <bitterns96@gmail.com>
2026-02-13 21:36:50 +0900

    feat: add shortcut handler

--- contexts/PanelProvider.tsx
+++ contexts/PanelProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export type PanelState = boolean | "";
 interface PanelContextType {
@@ -8,6 +8,9 @@ interface PanelContextType {
   showFooter: PanelState;
   setShowAside: React.Dispatch<React.SetStateAction<PanelState>>;
   setShowFooter: React.Dispatch<React.SetStateAction<PanelState>>;
+  toggleAside: () => void;
+  toggleFooter: () => void;
+  closeMobileAside: () => void;
 }
 
 const PanelContext = createContext<PanelContextType | null>(null);
@@ -17,42 +20,67 @@ export function usePanel() {
   if (!panelState) {
     throw new Error("usePanel must be used within a PanelProvider.");
   }
-
-  function footerCb(prev: boolean | "") {
-    if (prev === "") {
-      return true;
-    }
-    return !prev;
-  }
-
-  function asideCb(prev: boolean | "") {
-    if (prev === "") {
-      // 데스크톱: false(닫기), 모바일: true(열기)
-      return !(window.innerWidth > 768);
-    }
-    return !prev;
-  }
-
-  function closeMobileAside() {
-    if (!(window.innerWidth > 768)) {
-      panelState?.setShowAside(false);
-    }
-  }
-
-  return {
-    ...panelState,
-    toggleFooter: () => panelState.setShowFooter(footerCb),
-    toggleAside: () => panelState.setShowAside(asideCb),
-    closeMobileAside
-  };
+  return panelState;
 }
 
 export default function PanelProvider({ children }: { children: React.ReactNode; }) {
   const [showAside, setShowAside] = useState<PanelState>("");
   const [showFooter, setShowFooter] = useState<PanelState>("");
 
+  function toggleFooter() {
+    setShowFooter((prev: boolean | "") => {
+      if (prev === "") {
+        return true;
+      }
+      return !prev;
+    });
+  }
+
+  function toggleAside() {
+    setShowAside((prev: boolean | "") => {
+      if (prev === "") {
+        // 데스크톱: false(닫기), 모바일: true(열기)
+        return !(window.innerWidth > 768);
+      }
+      return !prev;
+    });
+  }
+
+  function closeMobileAside() {
+    if (!(window.innerWidth > 768)) {
+      setShowAside(false);
+    }
+  }
+
+  useEffect(() => {
+    function onKeyDownCb(e: KeyboardEvent) {
+      if ((e.ctrlKey || e.metaKey)) {
+        e.preventDefault();
+        const key = e.key;
+        switch (key) {
+          case "b":
+            return toggleAside();
+          case "j":
+            return toggleFooter();
+        }
+      }
+    }
+    document.addEventListener("keydown", onKeyDownCb);
+    return () => {
+      document.removeEventListener("keydown", onKeyDownCb);
+    };
+  }, []);
+
   return (
-    <PanelContext.Provider value={{ showAside, showFooter, setShowAside, setShowFooter }}>
+    <PanelContext.Provider value={{
+      showAside,
+      showFooter,
+      setShowAside,
+      setShowFooter,
+      toggleFooter,
+      toggleAside,
+      closeMobileAside
+    }}>
       {children}
     </PanelContext.Provider>
   );
