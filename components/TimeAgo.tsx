3c21e4dd7a411f78fae7e0ee0ffd8b82bc30858d
PollyGotACracker <bitterns96@gmail.com>
2026-02-15 07:12:04 +0900

    fix: fix mismatch text

--- components/TimeAgo.tsx
+++ components/TimeAgo.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useSyncExternalStore } from 'react';
+import { useEffect, useSyncExternalStore } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import "dayjs/locale/ko";
@@ -13,13 +13,6 @@ function subscribe(listener: () => void) {
   listeners.add(listener);
   return () => listeners.delete(listener);
 }
-// 서버 환경에서 window 를 직접 비교하게 되는 경우 에러 발생
-// interval로 리렌더 트리거, 실제 값 변경은 없음
-if (typeof window !== 'undefined') {
-  setInterval(() => {
-    listeners.forEach(listener => listener());
-  }, 60 * 1000);
-}
 
 export default function TimeAgo({ date, locale = "ko" }: { date: string; locale?: string; }) {
   /**
@@ -31,6 +24,13 @@ export default function TimeAgo({ date, locale = "ko" }: { date: string; locale?
     () => dayjs(date).locale(locale).fromNow(), // client
     () => '' // server (empty string)
   );
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      listeners.forEach(listener => listener());
+    }, 60 * 1000);
+    return () => clearInterval(id);
+  }, []);
 
   return <span>{timeAgo}</span>;
 }
